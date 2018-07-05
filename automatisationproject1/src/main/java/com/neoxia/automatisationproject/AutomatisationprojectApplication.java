0f4a4f88a8f7a0c16ec183ad6ad0ec989b37dea0
chaimaerakhim <chaimaarakhime@gmail.com>
2018-07-05 17:31:19 +0100

    project creation

---
+++ automatisationproject1/src/main/java/com/neoxia/automatisationproject/AutomatisationprojectApplication.java
@@ -0,0 +1,12 @@
+package com.neoxia.automatisationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatisationprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatisationprojectApplication.class, args);
+    }
+}
